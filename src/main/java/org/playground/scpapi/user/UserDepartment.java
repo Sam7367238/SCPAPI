4eda054455cad8cf6b94d12d397aad1a60ce6d13
Ayman <aymantheremkus11@gmail.com>
2026-09-16 22:02:19 +0300

    Add department property to user entity

--- src/main/java/org/playground/scpapi/user/UserDepartment.java
+++ src/main/java/org/playground/scpapi/user/UserDepartment.java
@@ -3,6 +3,7 @@ package org.playground.scpapi.user;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
+import org.playground.scpapi.department.Department;
 
 import java.time.LocalDateTime;
 import java.util.UUID;
@@ -19,6 +20,10 @@ public class UserDepartment {
     @ManyToOne
     @JoinColumn(name = "user_id")
     private User user;
+
+    @ManyToOne
+    @JoinColumn(name = "department_id")
+    private Department department;
 
     @Column(name = "joined")
     private LocalDateTime joined;
