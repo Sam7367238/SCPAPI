0eea9cc0a5a5c02b80fc3771653ce4abd3cfb840
Ayman <aymantheremkus11@gmail.com>
2026-09-17 19:17:02 +0300

    Remove unused imports

--- src/main/java/org/playground/scpapi/common/GlobalExceptionHandler.java
+++ src/main/java/org/playground/scpapi/common/GlobalExceptionHandler.java
@@ -1,9 +1,7 @@
 package org.playground.scpapi.common;
 
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
-import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
